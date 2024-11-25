c5cc4d032f5864bd527c278b30549dde2ebaba8f
Gal <galhamo114@gmail.com>
2024-11-25 10:21:01 +0200

    Tried to add nested loop in order to create 16 x 16 grid - it didnt worked yet.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const NUM_OF_DIVS = 16
+
 const gridContainer = document.createElement('div')
 gridContainer.style.width = '100vw'
 gridContainer.style.height = '100vh'
@@ -6,3 +8,17 @@ gridContainer.style.border = '1em solid red'
 document.body.style.margin = '0'
 
 document.body.appendChild(gridContainer)
+
+const chiledDiv = document.createElement('div')
+chiledDiv.style.width = 'calc(100vw / 16)'
+chiledDiv.style.height = 'calc(100vh / 16)'
+chiledDiv.style.border = '5px solid green'
+
+
+for (let xAxis = 0; xAxis < NUM_OF_DIVS; xAxis++){
+    console.log(xAxis, 'X')
+    document.body.appendChild(chiledDiv)
+    for (let yAxis = 0; yAxis < NUM_OF_DIVS; yAxis++)
+        console.log(yAxis, 'Y')
+        gridContainer.appendChild(chiledDiv)
+}
