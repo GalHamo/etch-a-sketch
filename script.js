19b7f632ec0478729d159ee9e7db89dfabfa38ce
Gal <galhamo114@gmail.com>
2024-11-25 10:00:48 +0200

    Added html inside the javascript i created a big container for the grid
    I added the css just to make sure everything stays
    in the border of the screen

---
+++ script.js
@@ -0,0 +1,8 @@
+const gridContainer = document.createElement('div')
+gridContainer.style.width = '100vw'
+gridContainer.style.height = '100vh'
+gridContainer.style.border = '1em solid red'
+
+document.body.style.margin = '0'
+
+document.body.appendChild(gridContainer)
