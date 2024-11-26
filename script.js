23c2b95bfee93f9e4712dd8f4e52f59f8fc035be
Gal <galhamo114@gmail.com>
2024-11-26 10:04:53 +0200

    changed the logic of how the sizeof the play screen is set, by changing the static 16px by 16px to the user choice

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const NUM_OF_DIVS = 16
 document.body.style.margin = '0'
+let xAxisSquares = prompt('How much squares on the X axis')
+let yAxisSquares = prompt('How much squares on the Y axis')
 
 const gridContainer = document.createElement('div')
 gridContainer.style.width = '100vw'
@@ -10,15 +12,15 @@ gridContainer.style.flexDirection = 'column'
 
 document.body.appendChild(gridContainer)
 
-for (let xAxis = 0; xAxis < NUM_OF_DIVS; xAxis++){
+for (let xAxis = 0; xAxis < xAxisSquares; xAxis++){
     console.log(xAxis, 'X')
     const rowContainer = document.createElement('div')
     rowContainer.style.width = '100%'
-    rowContainer.style.height = 'calc(100% / 16)'
+    rowContainer.style.height = `calc(100% / ${xAxisSquares})`
     rowContainer.style.border = '1px solid black'
     rowContainer.style.display = 'flex'
     gridContainer.appendChild(rowContainer)
-    for (let yAxis = 0; yAxis < NUM_OF_DIVS; yAxis++){
+    for (let yAxis = 0; yAxis < yAxisSquares; yAxis++){
         console.log(yAxis, 'Y')
         const chiledDiv = document.createElement('div')
         chiledDiv.classList.add('green-box')
@@ -30,6 +32,7 @@ for (let xAxis = 0; xAxis < NUM_OF_DIVS; xAxis++){
         rowContainer.appendChild(chiledDiv)
         chiledDiv.addEventListener('mouseover', (selfChiled) => {
             if (!chiledDiv.classList.contains('green-bg')){
+                chiledDiv.classList.remove('white-bg')
                 chiledDiv.classList.add('green-bg')
             } else {
                 chiledDiv.classList.remove('green-bg')
